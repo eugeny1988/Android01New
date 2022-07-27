789c0b3a5a10831647211ecb18e410bb5ab2aa95
eugeny1988 <eugeny1988@gmail.com>
2022-07-27 14:46:24 +0300

    Добавил возможность поставить лайк

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        imageView.setOnClickListener {
+            binding.imageView.setImageResource(R.drawable.ic_baseline_thumb_up_alt_24)
+            var newLikes = R.string.likes
+            newLikes++
+        }
+
+
     }
 }
