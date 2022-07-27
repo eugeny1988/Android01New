973106ad209990b33ade68ec9128b638a6333348
eugeny1988 <eugeny1988@gmail.com>
2022-07-27 23:23:32 +0300

    Пытаюсь добавить счётчик лайков

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,21 +1,35 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var isClicked = false
+        var likes = R.string.likes
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         imageView.setOnClickListener {
-            binding.imageView.setImageResource(R.drawable.ic_baseline_thumb_up_alt_24)
-            var newLikes = R.string.likes
-            newLikes++
+            let {
+                if (!isClicked) {
+                    binding.imageView.setImageResource(R.drawable.ic_baseline_thumb_up_alt_24)
+                    isClicked = true
+                    likes++
+                    textViewLikes.setText(likes.toString())
+                } else {
+                    binding.imageView.setImageResource(R.drawable.ic_baseline_thumb_up_off_alt_24)
+                    likes--
+                    textViewLikes.setText(likes.toString())
+                }
+            }
+
+
         }
+
+        textViewLikes.setText(likes++)
 
 
     }
