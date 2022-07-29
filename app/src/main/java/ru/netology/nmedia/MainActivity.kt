739d01653e5ab62c70a69af5154642ada6d92992
eugeny1988 <eugeny1988@gmail.com>
2022-07-29 22:37:46 +0300

    Счётчик лайков

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var isClicked = false
         var likes = R.string.likes
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         imageView.setOnClickListener {
             let {
@@ -18,19 +19,26 @@ class MainActivity : AppCompatActivity() {
                     binding.imageView.setImageResource(R.drawable.ic_baseline_thumb_up_alt_24)
                     isClicked = true
                     likes++
-                    textViewLikes.setText(likes.toString())
+                    binding.textViewLikes.setText(viewClicks(likes))
                 } else {
                     binding.imageView.setImageResource(R.drawable.ic_baseline_thumb_up_off_alt_24)
                     likes--
-                    textViewLikes.setText(likes.toString())
+                    isClicked = false
+                    binding.textViewLikes.setText(viewClicks(likes))
                 }
             }
 
 
         }
+    }
 
-        textViewLikes.setText(likes++)
 
-
+    fun viewClicks(likes: Int): String {
+        return when (likes) {
+            in 0..1099 -> likes.toString()
+            in 1100..10_000 -> "${round((likes / 100).toDouble()) / 10}K"
+            in 10_001..999_999 -> "${round((likes / 1000).toDouble()) / 10}K"
+            else -> "${round((likes / 100_000).toDouble()) / 10}M"
+        }
     }
 }
