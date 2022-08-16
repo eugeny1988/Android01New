919b2dc3a8f5b042775cae4e62d318dbd77c2049
eugeny1988 <eugeny1988@gmail.com>
2022-08-16 12:25:55 +0300

    Добавил PostAdapter.kt

---
+++ app/src/main/java/ru/netology/nmedia/PostAdapter.kt
@@ -0,0 +1,28 @@
+package ru.netology.nmedia
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.databinding.ActivityMainBinding
+
+class PostAdapter : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    class ViewHolder(private val binding: ActivityMainBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflator = LayoutInflater.from(parent.context)
+        val binding = ActivityMainBinding.inflate(inflator,parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
