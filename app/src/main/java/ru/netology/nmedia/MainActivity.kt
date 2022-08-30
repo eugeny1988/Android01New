633bdcb2cd1aee8dd8498b33637e29cd10bb1cf8
eugeny1988 <eugeny1988@gmail.com>
2022-08-30 16:15:01 +0300

    Не понимаю, почему container красный

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,12 +3,9 @@ package ru.netology.nmedia
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.forEach
-import kotlinx.android.synthetic.main.post_layout.view.*
 import ru.netology.nmedia.data.PostRepositoryImplementation
 import ru.netology.nmedia.data.PostViewModel
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.PostLayoutBinding
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<PostViewModel>()
@@ -27,44 +24,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.container
         var postRepository = PostRepositoryImplementation()
         setContentView(binding.root)
 
         viewModel.data.observe(this) {
-            postRepository.posts.forEach { post ->
-                PostLayoutBinding.inflate(layoutInflater, binding.container, true)
-                    .apply {
-                        binding.container.name.setText(postRepository.posts[index].author)
-                        binding.container.date.setText(postRepository.posts[index].published)
-                        binding.container.textViewLikes.setText(
-                            postRepository.posts[index].viewNumbers(
-                                postRepository.posts[index].postLikes
-                            ).toString()
-                        )
-                        binding.container.textViewShares.setText(
-                            postRepository.posts[index].viewNumbers(
-                                postRepository.posts[index].postShares
-                            ).toString()
-                        )
-                        binding.container.textViewMessage.setText(postRepository.posts[index].content)
-                        imageViewLikes.setOnClickListener {
-                            viewModel.onLikeClicked(post)
-                        }
-                        imageViewShare.setOnClickListener {
-                            viewModel.onShareClicked(post)
-                            if (postRepository.posts[index].isLiked) imageViewLikes.setImageResource(
-                                R.drawable.ic_baseline_thumb_up_alt_24
-                            )
-                            else
-                                imageViewLikes.setImageResource(R.drawable.ic_baseline_thumb_up_off_alt_24)
-
-                        }
-                    }
-                    .root
+            val adapter = PostAdapter(postRepository.posts) {
+                viewModel.onLikeClicked(it)
             }
-
+            binding.postRecyclerView.adapter = adapter
         }
-    }
 
+    }
 }
--- app/src/main/java/ru/netology/nmedia/PostAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/PostAdapter.kt
@@ -1,31 +1,52 @@
 package ru.netology.nmedia
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.Post
+import ru.netology.nmedia.*
+import ru.netology.nmedia.data.*
+import ru.netology.nmedia.databinding.PostLayoutBinding
 
-class PostAdapter (
-    val posts : List<Post>
+class PostAdapter(
+    private val posts: List<Post>,
+    private val onLikeClicked: (Post) -> Unit
 ) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
-    class ViewHolder(private val binding: ActivityMainBinding) :
-        RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(
+        private val binding: PostLayoutBinding,
+        private val onLikeClicked: (Post) -> Unit
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        val postRepository = PostRepositoryImplementation()
+        fun bind(post: Post) = with(binding) {
+            binding.container.name.setText(post.author)
+            binding.container.date.setText(post.published)
+            binding.container.textViewLikes.setText(
+                post.viewNumbers(
+                    post.postLikes
+                )
+            )
+            binding.container.textViewShares.setText(
+                post.viewNumbers(
+                    post.postShares
+                )
+            )
+            binding.container.textViewMessage.setText(post.content)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflator = LayoutInflater.from(parent.context)
-        val binding = ActivityMainBinding.inflate(inflator,parent,false)
+        val binding = PostLayoutBinding.inflate(inflator, parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val post = posts[position]
+        holder.bind(post)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = posts.size
+
 
 }
