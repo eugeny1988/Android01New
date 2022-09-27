39c6944c230ddf61090c5c14cba8ca96d709bcc3
eugeny1988 <eugeny1988@gmail.com>
2022-09-27 10:31:35 +0300

    Could not find or load main class worker.org.gradle.process.internal.worker.GradleWorkerMain

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,34 +2,25 @@ package ru.netology.nmedia
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.data.PostRepositoryImplementation
-import ru.netology.nmedia.data.PostViewModel
+import ru.netology.nmedia.Post
+import ru.netology.nmedia.PostAdapter
+import ru.netology.nmedia.data.*
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.PostLayoutBinding
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by viewModels<PostViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        var postRepository = PostRepositoryImplementation()
-        var posts = postRepository.posts
         setContentView(binding.root)
-        val adapter = PostAdapter() {
-            viewModel::onLikeClicked
-            viewModel::onShareClicked
+
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostAdapter {
+            viewModel.onLikeClicked(it)
         }
-        var bindingPost = PostLayoutBinding.inflate(layoutInflater)
-
-//        binding.postRecyclerView.adapter = adapter
-
-        viewModel.data.observe(this) {
-            adapter.posts = posts
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.posts = posts as MutableList<Post>
         }
-
     }
 }
